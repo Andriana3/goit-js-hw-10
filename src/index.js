a774bd5cc9fc03d5b4cb3bcec136c43b8dccbbbf
Andriana3 <andrianavovchok@gmail.com>
2023-11-06 15:17:03 +0100

    update

--- src/index.js
+++ src/index.js
@@ -8,22 +8,21 @@ const errLoader = document.querySelector('.loader');
 const errElement = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
 const breedSelect = document.querySelector('.breed-select');
+errLoader.textContent = '';
 
 
 
 fetchBreeds()
     .then(breeds => {
         hideLoader();
-        
-    breedSelect.classList.remove('hide');
-    breedSelect.classList.add('show');
+        showCatInfo()
+    breedSelect.classList.remove('is-hidden');
         
     breeds.forEach(breed => {
       const option = document.createElement('option');
       option.value = breed.id;
       option.textContent = breed.name;
       breedSelect.appendChild(option);
-
     });
         
         const breedSelectInstance = new SlimSelect({
@@ -34,7 +33,8 @@ fetchBreeds()
     
     .catch(error => {
       hideLoader();
-        showError();
+      showError();
+      hideCatInfo()
        console.log("помилка")
   });
 
@@ -42,44 +42,54 @@ fetchBreeds()
 
 breedSelect.addEventListener('change', () => {
   const selectedBreedId = breedSelect.value;
-    showLoader()
+  showLoader()
+hideCatInfo()
     
-
-  fetchCatByBreed(selectedBreedId)
-    .then(catData => {
+  if (selectedBreedId) {
+    fetchCatByBreed(selectedBreedId)
+      .then(catData => {
         hideLoader();
         hideError()
-        
+        showCatInfo()
 
-      catInfo.innerHTML = `
+        catInfo.innerHTML = `
         <img src="${catData[0].url}" alt="Зображення кота" width = "500px" heigth = "500px">
         <h2>Назва породи: ${catData[0].breeds[0].name}</h2>
         <p>Опис: ${catData[0].breeds[0].description}</p>
         <p>Темперамент: ${catData[0].breeds[0].temperament}</p>
       `;
-    })
-    .catch(error => {
-     console.log("помилка")
-    });
+      })
+      .catch(error => {
+        console.log("помилка")
+        showError()
+        hideCatInfo()
+      });
+  }
 });
 
 
 function showLoader() {
-  errLoader.classList.remove('hide');
-  errLoader.classList.add('show');
+  errLoader.classList.remove('is-hidden');
+  errLoader.textContent = '';
 }
 
 function hideLoader() {
-  errLoader.classList.remove('show');
-  errLoader.classList.add('hide');
+  errLoader.classList.add('is-hidden');
 }
 
 function showError() {
-  errElement.classList.remove('hide');
-  errElement.classList.add('show');
+  errElement.classList.remove('is-hidden');
+  
 }
 
 function hideError() {
-  errElement.classList.remove('show');
-  errElement.classList.add('hide');
+  errElement.classList.add('is-hidden');
+}
+
+function showCatInfo() {
+  catInfo.classList.remove('is-hidden');
+}
+
+function hideCatInfo() {
+  catInfo.classList.add('is-hidden');
 }
